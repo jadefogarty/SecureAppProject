9775b158532491737febe30e68ed41df64a3b6c5
jadefogarty <jadefogarty@gmail.com>
2024-03-29 01:34:46 +0000

    Updating review validation

--- validation/reviewValidator.js
+++ validation/reviewValidator.js
@@ -1,8 +1,16 @@
 const { check } = require('express-validator');
 
-// Export function validateTask
+// Export function validateReview
 exports.validateReview = [
-  check('Review')
-  .isLength({ min:2, max: 120 })
-  .withMessage("Fields must have between 2 and 120 characters")
+  check('BookTitle')
+  .isLength({ min:2, max: 60 })
+  .withMessage("Field must be between 2 and 60 characters"),
+
+  check('BookAuthor')
+  .isLength({ min: 2, max: 30 })
+  .withMessage('Field must be between 2 and 40 characters'),
+
+check('Comments')
+  .isLength({ min: 2, max: 120 })
+  .withMessage('Field must be between 2 and 120 characters'),
 ];
